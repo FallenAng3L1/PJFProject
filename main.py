849cc65766329987e5f94f8276f8306e65aaad13
Damian <damian.michrowski@gmail.com>
2022-01-14 20:58:28 +0100

    added functionality to keep scrolling infinite pages

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from agoda.agoda import Agoda
-
+import agoda.datacsv as d_csv
+import time as t
 with Agoda() as bot:
     bot.land_first_page()
     bot.accept_popup()
     bot.select_place('Warszawa')
-    bot.select_dates(check_in_date='Wed Jan 12 2022', check_out_date='Tue Feb 01 2022')
+    bot.select_dates(check_in_date='Sat Jan 15 2022', check_out_date='Fri Jan 21 2022')
     bot.select_guests(3,2,4,2)
     bot.search_click()
-    bot.scroll_down()
-    bot.report_results()
+    while True:
+        try:
+            bot.scroll_down()
+            bot.report_results()
+        except Exception:
+            bot.scroll_down()
+            bot.report_results()
+        try:
+            bot.next_page()
+            t.sleep(5)
+        except Exception:
+            d_csv.data_printer()
+            break
