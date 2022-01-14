753c785ad839c9a1b273b0f6cadfa2353c894821
Damian <damian.michrowski@gmail.com>
2022-01-14 21:00:25 +0100

    rearranged functions for working with infinite pages

--- agoda/agoda_results.py
+++ agoda/agoda_results.py
@@ -1,6 +1,7 @@
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebElement
+import selenium.common.exceptions as ex
+import agoda.constants as const
 class AgodaReport:
     def __init__(self, boxes_section_element:WebElement):
         self.boxes_section_element = boxes_section_element
@@ -16,29 +17,24 @@ class AgodaReport:
             try:
                 hotel_name = deal_box.find_element(By.CLASS_NAME, 'PropertyCard__HotelName').get_attribute('innerHTML').strip()
                 print(hotel_name)
-            except Exception:
-                self.execute_script("window.scrollBy(0, 3000)")
-                hotel_name = deal_box.find_element(By.CLASS_NAME, 'PropertyCard__HotelName').get_attribute(
-                    'innerHTML').strip()
-                print(hotel_name)
+            except ex.NoSuchElementException:
+                hotel_name = deal_box.find_element(By.CLASS_NAME, 'PropertyCard__HotelName').get_attribute('innerHTML').strip()
             hotel_names.append(hotel_name)
-        return hotel_names
+        const.hotel_names_all += hotel_names
 
     def pull_prices(self):
         prices = []
         for deal_box in self.deal_boxes:
             price_tag = deal_box.find_element(By.CLASS_NAME, 'PropertyCardPrice__Value').get_attribute('innerHTML').strip()
-            print(price_tag)
             prices.append(price_tag)
-        return prices
+        const.hotel_prices_all += prices
 
     def review_score(self):
         review_scores = []
         for deal_box in self.deal_boxes:
             try:
                 score = deal_box.find_element(By.CLASS_NAME, 'Hkrzy').get_attribute('innerHTML').strip()
-                print(score)
                 review_scores.append(score)
             except Exception:
                 review_scores.append("NA")
-        return review_scores
+        const.hotel_rating_all += review_scores
