3b1a2bdad8cd37c7e61b754cdfa6cea93ccf7569
Damian <damian.michrowski@gmail.com>
2022-01-20 10:47:09 +0100

    updated for making csv for declared amount of properties

--- main.py
+++ main.py
@@ -1,23 +1,28 @@
 from agoda.agoda import Agoda
 import agoda.datacsv as d_csv
 import time as t
+properties = ["Śniadanie", "winda"]
 with Agoda() as bot:
     bot.land_first_page()
     bot.accept_popup()
     bot.select_place('Warszawa')
-    bot.select_dates(check_in_date='Sat Jan 15 2022', check_out_date='Fri Jan 21 2022')
-    bot.select_guests(3,2,4,2)
+    bot.select_dates(check_in_date='Sat Mar 05 2022', check_out_date='Sat Mar 26 2022')
+    bot.select_guests(3,2,4,2,[5,13])
     bot.search_click()
     while True:
         try:
             bot.scroll_down()
+            bot.scroll_properties(properties)
             bot.report_results()
+
         except Exception:
             bot.scroll_down()
+            bot.scroll_properties(properties)
             bot.report_results()
+
         try:
             bot.next_page()
             t.sleep(5)
         except Exception:
-            d_csv.data_printer()
+            d_csv.data_printer(properties)
             break
