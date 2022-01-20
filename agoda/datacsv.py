22341cd0b53090a7acc8c8aa650d595070d0f7c3
Damian <damian.michrowski@gmail.com>
2022-01-20 10:46:58 +0100

    updated for making csv for declared amount of properties

--- agoda/datacsv.py
+++ agoda/datacsv.py
@@ -1,8 +1,10 @@
 import agoda.constants as const
 import pandas as pd
-def data_printer():
+def data_printer(properties):
     data = {'Hotel Name': const.hotel_names_all,
             'Price': const.hotel_prices_all,
             'Rating': const.hotel_rating_all}
     df = pd.DataFrame(data)
+    for prop in range(len(properties)):
+        df[f'{const.hotel_breakfast_all[prop][0]}'] = const.hotel_breakfast_all[prop][1:]
     df.to_csv('hotels.csv', index=False)
