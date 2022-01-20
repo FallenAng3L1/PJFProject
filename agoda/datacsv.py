b174c22ad89d47d8072a0251df4d79f072b21642
Damian <damian.michrowski@gmail.com>
2022-01-20 10:48:07 +0100

    changed name

--- agoda/datacsv.py
+++ agoda/datacsv.py
@@ -6,5 +6,5 @@ def data_printer(properties):
             'Rating': const.hotel_rating_all}
     df = pd.DataFrame(data)
     for prop in range(len(properties)):
-        df[f'{const.hotel_breakfast_all[prop][0]}'] = const.hotel_breakfast_all[prop][1:]
+        df[f'{const.hotel_properties_all[prop][0]}'] = const.hotel_properties_all[prop][1:]
     df.to_csv('hotels.csv', index=False)
